3dcd70ef00b07b7e8219517ba7beedfb6e236444
John Hurliman <jhurliman@jhurliman.org>
2023-02-21 11:54:24 -0800

    lint

--- internal/schemas.ts
+++ internal/schemas.ts
@@ -785,7 +785,8 @@ const CompressedImage: FoxgloveMessageSchema = {
     {
       name: "format",
       type: { type: "primitive", name: "string" },
-      description: "Image format\n\nSupported values: `webp`, `jpeg`, `png`, `zfp`\n\nFor ZFP compression, only 2D fields are supported and the data must be prefixed with a full ZFP header (`ZFP_HEADER_FULL`).",
+      description:
+        "Image format\n\nSupported values: `webp`, `jpeg`, `png`, `zfp`\n\nFor ZFP compression, only 2D fields are supported and the data must be prefixed with a full ZFP header (`ZFP_HEADER_FULL`).",
     },
   ],
 };

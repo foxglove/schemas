58cab92620602dccc07502e8b79114bd060bb965
John Hurliman <jhurliman@jhurliman.org>
2023-06-20 15:30:35 -0700

    Add foxglove.CompressedVideo

--- internal/schemas.ts
+++ internal/schemas.ts
@@ -790,6 +790,51 @@ const CompressedImage: FoxgloveMessageSchema = {
   ],
 };
 
+const CompressedVideo: FoxgloveMessageSchema = {
+  type: "message",
+  name: "CompressedVideo",
+  description: "A single frame of a compressed video bitstream",
+  fields: [
+    {
+      name: "timestamp",
+      type: { type: "primitive", name: "time" },
+      description: "Timestamp of video frame",
+    },
+    {
+      name: "frame_id",
+      type: { type: "primitive", name: "string" },
+      description:
+        "Frame of reference for the decoded image. The origin of the frame is the optical center of the camera. +x points to the right in the image, +y points down, and +z points into the plane of the image.",
+    },
+    {
+      name: "data",
+      type: { type: "primitive", name: "bytes" },
+      description:
+        "Compressed video frame data. For packet-based video codecs this data must begin and end on packet boundaries (no partial packets), and must contain enough video packets to decode exactly one image (either a keyframe or delta frame).",
+    },
+    {
+      name: "keyframe",
+      type: { type: "primitive", name: "boolean" },
+      description:
+        "Indicates whether this video frame is a keyframe or delta frame. In H.26x terminology these are also known as I-frames (intra-coded picture) and P-frames (predicted picture). B-frames are not supported. When this field is set to true, the `metadata` array must be populated as well.",
+    },
+    {
+      name: "metadata",
+      type: { type: "nested", schema: KeyValuePair },
+      array: true,
+      description: `Video metadata used to initialize a video decoder, expressed as an array of key-value pairs. This metadata must be present for every keyframe (when \`keyframe\` is true), and should be left empty for delta frames. This metadata is modeled after [WebCodecs VideoDecoderConfig](https://www.w3.org/TR/webcodecs/#video-decoder-config), although it can be used to initialize any video decoder. See the See the [WebCodecs Codec Registry](https://www.w3.org/TR/webcodecs-avc-codec-registration/) for more information on the interpretation of each field for a specific codec. The following keys are supported:
+
+      * \`codec\` - A fully qualified codec string describing the video codec. The format for this string is specific to each codec, and is described in the WebCodecs Codec Registry.
+      * \`description\` - Base64-encoded binary data that is specific to each codec. For H.264 this is an AVCDecoderConfigurationRecord (avcC data), and for H.265 this is a HEVCDecoderConfigurationRecord (hvcC data).
+      * \`codedWidth\` - Width of video frames in pixels, potentially including non-visible padding, and prior to considering potential ratio adjustments.
+      * \`codedHeight\` - Height of video frames in pixels, potentially including non-visible padding, and prior to considering potential ratio adjustments.
+      * \`displayAspectWidth\` - (Optional) Horizontal dimension of the VideoFrame's aspect ratio when displayed.
+      * \`displayAspectHeight\` - (Optional) Vertical dimension of the VideoFrame's aspect ratio when displayed.
+`,
+    },
+  ],
+};
+
 const RawImage: FoxgloveMessageSchema = {
   type: "message",
   name: "RawImage",
@@ -1362,6 +1407,7 @@ export const foxgloveMessageSchemas = {
   CircleAnnotation,
   Color,
   CompressedImage,
+  CompressedVideo,
   CylinderPrimitive,
   CubePrimitive,
   FrameTransform,
--- schemas/README.md
+++ schemas/README.md
@@ -17,6 +17,7 @@ All schemas are generated from [schemas.ts](/internal/schemas.ts).
 - [CircleAnnotation](#circleannotation)
 - [Color](#color)
 - [CompressedImage](#compressedimage)
+- [CompressedVideo](#compressedvideo)
 - [CubePrimitive](#cubeprimitive)
 - [CylinderPrimitive](#cylinderprimitive)
 - [FrameTransform](#frametransform)
@@ -608,6 +609,91 @@ string
 Image format
 
 Supported values: `webp`, `jpeg`, `png`
+
+</td>
+</tr>
+</table>
+
+## CompressedVideo
+
+A single frame of a compressed video bitstream
+
+<table>
+  <tr>
+    <th>field</th>
+    <th>type</th>
+    <th>description</th>
+  </tr>
+<tr>
+<td><code>timestamp</code></td>
+<td>
+
+time
+
+</td>
+<td>
+
+Timestamp of video frame
+
+</td>
+</tr>
+<tr>
+<td><code>frame_id</code></td>
+<td>
+
+string
+
+</td>
+<td>
+
+Frame of reference for the decoded image. The origin of the frame is the optical center of the camera. +x points to the right in the image, +y points down, and +z points into the plane of the image.
+
+</td>
+</tr>
+<tr>
+<td><code>data</code></td>
+<td>
+
+bytes
+
+</td>
+<td>
+
+Compressed video frame data. For packet-based video codecs this data must begin and end on packet boundaries (no partial packets), and must contain enough video packets to decode exactly one image (either a keyframe or delta frame).
+
+</td>
+</tr>
+<tr>
+<td><code>keyframe</code></td>
+<td>
+
+boolean
+
+</td>
+<td>
+
+Indicates whether this video frame is a keyframe or delta frame. In H.26x terminology these are also known as I-frames (intra-coded picture) and P-frames (predicted picture). B-frames are not supported. When this field is set to true, the `metadata` array must be populated as well.
+
+</td>
+</tr>
+<tr>
+<td><code>metadata</code></td>
+<td>
+
+[KeyValuePair](#keyvaluepair)[]
+
+</td>
+<td>
+
+Video metadata used to initialize a video decoder, expressed as an array of key-value pairs. This metadata must be present for every keyframe (when `keyframe` is true), and should be left empty for delta frames. This metadata is modeled after [WebCodecs VideoDecoderConfig](https://www.w3.org/TR/webcodecs/#video-decoder-config), although it can be used to initialize any video decoder. See the See the [WebCodecs Codec Registry](https://www.w3.org/TR/webcodecs-avc-codec-registration/) for more information on the interpretation of each field for a specific codec. The following keys are supported:
+
+      * `codec` - A fully qualified codec string describing the video codec. The format for this string is specific to each codec, and is described in the WebCodecs Codec Registry.
+      * `description` - Base64-encoded binary data that is specific to each codec. For H.264 this is an AVCDecoderConfigurationRecord (avcC data), and for H.265 this is a HEVCDecoderConfigurationRecord (hvcC data).
+      * `codedWidth` - Width of video frames in pixels, potentially including non-visible padding, and prior to considering potential ratio adjustments.
+      * `codedHeight` - Height of video frames in pixels, potentially including non-visible padding, and prior to considering potential ratio adjustments.
+      * `displayAspectWidth` - (Optional) Horizontal dimension of the VideoFrame's aspect ratio when displayed.
+      * `displayAspectHeight` - (Optional) Vertical dimension of the VideoFrame's aspect ratio when displayed.
+
 
 </td>
 </tr>

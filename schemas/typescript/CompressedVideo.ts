03f91d4c61bd91d8b23e4effa5e4f04850df789b
Sam Nosenzo <sam@foxglove.dev>
2023-09-29 13:43:04 -0400

    Update comments for `foxglove.CompressedVideo` schema (#129)
    Timestamp refers to video frame instead of image
    
    Added note to `data` field to state that B-frames should not be used.

--- schemas/typescript/CompressedVideo.ts
+++ schemas/typescript/CompressedVideo.ts
@@ -5,13 +5,13 @@ import { Time } from "./Time";
 
 /** A single frame of a compressed video bitstream */
 export type CompressedVideo = {
-  /** Timestamp of image */
+  /** Timestamp of video frame */
   timestamp: Time;
 
-  /** Frame of reference for the image. The origin of the frame is the optical center of the camera. +x points to the right in the image, +y points down, and +z points into the plane of the image. */
+  /** Frame of reference for the video. The origin of the frame is the optical center of the camera. +x points to the right in the video, +y points down, and +z points into the plane of the video. */
   frame_id: string;
 
-  /** Compressed video frame data. For packet-based video codecs this data must begin and end on packet boundaries (no partial packets), and must contain enough video packets to decode exactly one image (either a keyframe or delta frame). */
+  /** Compressed video frame data. For packet-based video codecs this data must begin and end on packet boundaries (no partial packets), and must contain enough video packets to decode exactly one image (either a keyframe or delta frame). Note: Foxglove Studio does not support video streams that include B frames because they require lookahead. */
   data: Uint8Array;
 
   /**

68ed324e47664011111591c5a03ade6db85943cd
Greg Smith <gasmith@foxglove.dev>
2025-02-05 13:29:01 -0800

    sort test too

--- rust/foxglove/src/websocket/unstable_tests.rs
+++ rust/foxglove/src/websocket/unstable_tests.rs
@@ -133,6 +133,33 @@ async fn test_client_advertising() {
 }
 
 #[tokio::test]
+async fn test_broadcast_time() {
+    let server = create_server(ServerOptions {
+        capabilities: Some(HashSet::from([Capability::Time])),
+        ..Default::default()
+    });
+    let addr = server
+        .start("127.0.0.1", 0)
+        .await
+        .expect("Failed to start server");
+
+    let mut ws_client = connect_client(addr).await;
+    _ = ws_client.next().await.expect("serverInfo");
+
+    server.broadcast_time(42).await;
+    let msg = ws_client
+        .next()
+        .await
+        .expect("no message received")
+        .expect("failed to parse message");
+    let Message::Binary(mut buf) = msg else {
+        panic!("unexpected message type");
+    };
+    assert_eq!(buf.get_u8(), protocol::server::BinaryOpcode::TimeData as u8);
+    assert_eq!(buf.get_u64_le(), 42);
+}
+
+#[tokio::test]
 async fn test_parameter_values() {
     let server = create_server(ServerOptions {
         capabilities: Some(HashSet::from([Capability::Parameters])),
@@ -170,31 +197,4 @@ async fn test_parameter_values() {
     assert_eq!(msg["parameters"][0]["value"], 1.23);
 
     server.stop().await;
-}
-
-#[tokio::test]
-async fn test_time() {
-    let server = create_server(ServerOptions {
-        capabilities: Some(HashSet::from([Capability::Time])),
-        ..Default::default()
-    });
-    let addr = server
-        .start("127.0.0.1", 0)
-        .await
-        .expect("Failed to start server");
-
-    let mut ws_client = connect_client(addr).await;
-    _ = ws_client.next().await.expect("serverInfo");
-
-    server.broadcast_time(42).await;
-    let msg = ws_client
-        .next()
-        .await
-        .expect("no message received")
-        .expect("failed to parse message");
-    let Message::Binary(mut buf) = msg else {
-        panic!("unexpected message type");
-    };
-    assert_eq!(buf.get_u8(), protocol::server::BinaryOpcode::TimeData as u8);
-    assert_eq!(buf.get_u64_le(), 42);
 }
